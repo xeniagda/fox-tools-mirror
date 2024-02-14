aaacd40528bba5f76d9f6845bc5545341041b304
xenia <xenia.agda@gmail.com>
2024-02-14 22:09:51 +0100

    Update rev generation

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
           cargoLock = { lockFile = ./gfx2inc/Cargo.lock; };
 
           NIX_SEMVER = version;
-          NIX_GIT_SHA_SHORT = if self ? rev then builtins.substring 0 7 self.rev else "dirty";
+          NIX_GIT_SHA_SHORT = self.shortRev or self.dirtyShortRev;
         };
         packages.default = packages.gfx2inc;
 
